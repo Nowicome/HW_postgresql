0bd456ceec50f6da0f092386e61139813ed97c0a
Nowicome <125184525+Nowicome@users.noreply.github.com>
2023-06-09 14:59:12 +0600

    corrected some requests
    --Название треков, которые содержат слово «мой» или «my»;
    --Количество треков, вошедших в альбомы 2019–2020 годов;
    --Все исполнители, которые не выпустили альбомы в 2020 году;
    --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.

--- Select.sql
+++ Select.sql
@@ -23,7 +23,7 @@ WHERE name_or_alias NOT LIKE '% %';
 --Название треков, которые содержат слово «мой» или «my»
 SELECT name_of_the_track, duration
 FROM tracks
-WHERE lower(name_of_the_track) LIKE '%мой%' OR lower(name_of_the_track) LIKE '%my%';
+WHERE lower(name_of_the_track) SIMILAR TO '(my %|% my %|% my|мой %|% мой %|% мой)';
 
 --Задание 3
 --Количество исполнителей в каждом жанре
@@ -33,11 +33,9 @@ GROUP BY g.name_of_the_genre
 ORDER BY p DESC;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов
-SELECT title, count(name_of_the_track) n FROM albums a
-JOIN tracks t ON a.id = t.album_id
-WHERE EXTRACT (YEAR FROM year_of_issue) BETWEEN 2019 AND 2020
-GROUP BY a.title
-ORDER BY n DESC;
+SELECT count(name_of_the_track) n FROM tracks t
+JOIN albums a ON t.album_id = a.id
+WHERE EXTRACT (YEAR FROM year_of_issue) BETWEEN 2019 AND 2020;
 
 --Средняя продолжительность треков по каждому альбому
 SELECT title, avg(duration) d FROM albums a
@@ -46,11 +44,13 @@ GROUP BY a.title
 ORDER BY d DESC;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году
-SELECT name_or_alias FROM performers p
-JOIN performer_album pa ON p.id = pa.performer_id
-JOIN albums a ON pa.album_id = a.id
-WHERE NOT EXTRACT (YEAR FROM a.year_of_issue) = 2020
-GROUP BY p.name_or_alias
+SELECT name_or_alias FROM performers
+WHERE name_or_alias NOT IN (
+	SELECT name_or_alias FROM performers p
+	JOIN performer_album pa ON p.id = pa.performer_id
+	JOIN albums a ON pa.album_id = a.id
+	WHERE EXTRACT (YEAR FROM a.year_of_issue) = 2020
+	);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
 SELECT DISTINCT name_of_the_collection FROM collections co
@@ -63,15 +63,12 @@ WHERE p.name_or_alias = 'Rammstein'
 
 --Задание 4(необязательное)
 --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-SELECT DISTINCT title FROM albums a
+SELECT DISTINCT a.title FROM albums a
 JOIN performer_album pa ON a.id = pa.album_id
 JOIN performers p ON pa.performer_id = p.id
 JOIN genre_performer gp ON p.id = gp.performer_id
-JOIN genres g ON gp.genre_id = g.id
-GROUP BY a.title
-HAVING count(DISTINCT g.name_of_the_genre) > 1
-ORDER BY a.title
-
+GROUP BY a.title, gp.performer_id
+HAVING count(gp.genre_id) > 1;
 
 --Наименования треков, которые не входят в сборники
 SELECT name_of_the_track FROM tracks t
